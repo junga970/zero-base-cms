929666bd8b279e40919d62488c7286ab71353d15
junga970 <junga2940@gmail.com>
2023-02-06 15:41:45 +0900

    feat : [016-PAYMENT] 결제 기능 추가

--- order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
+++ order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
@@ -12,7 +12,9 @@ public enum ErrorCode {
 	PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST, "상품을 찾을 수 없습니다."),
 	ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "아이템을 찾을 수 없습니다."),
 	ITEM_COUNT_NOT_ENOUGH(HttpStatus.BAD_REQUEST, "상품 수량이 부족합니다."),
-	KART_CHANGE_FAIL(HttpStatus.BAD_REQUEST, "장바구니에 추가할 수 없습니다.");
+	KART_CHANGE_FAIL(HttpStatus.BAD_REQUEST, "장바구니에 추가할 수 없습니다."),
+	ORDER_FAIL_CHECK_CART(HttpStatus.BAD_REQUEST, "주문 불가! 장바구니를 확인해주세요."),
+	ORDER_FAIL_NO_MONEY(HttpStatus.BAD_REQUEST, "주문 불가! 잔액이 부족합니다.");
 
 	private final HttpStatus httpStatus;
 	private final String detail;
