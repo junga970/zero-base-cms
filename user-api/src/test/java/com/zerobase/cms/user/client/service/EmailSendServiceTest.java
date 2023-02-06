17b276021502ae26801e4c881ee9c08adf0b77d1
junga970 <junga2940@gmail.com>
2023-02-06 11:22:42 +0900

    feat : [003-EMAIL-VERIFICATION] 고객 이메일 verification

--- user-api/src/test/java/com/zerobase/cms/user/client/service/EmailSendServiceTest.java
+++ user-api/src/test/java/com/zerobase/cms/user/client/service/EmailSendServiceTest.java
@@ -1,20 +1,24 @@
-package com.zerobase.cms.user.client.service;
 
-import com.zerobase.cms.user.client.MailgunClient;
+package com.zerobase.cms.user.client;
+
+import com.zerobase.cms.user.client.mailgun.SendMailForm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmailSendServiceTest {
+public class EmailSendServiceTest {
 
 	@Autowired
 	private MailgunClient mailgunClient;
 
 	@Test
-	void test() {
-//		String response = emailSendService.sendEmail();
-//		System.out.println(response);
-
+	public void EmailTest() {
+		mailgunClient.sendEmail(SendMailForm.builder()
+			.from("zerobase-test@gmail.com")
+			.to("wjddk2940@gmail.com")
+			.text("mail test zerobase")
+			.subject("test")
+			.build());
 	}
 }
