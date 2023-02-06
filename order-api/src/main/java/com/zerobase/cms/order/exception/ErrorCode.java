0bba632e852595155ee25f9597b6342283dcdad6
junga970 <junga2940@gmail.com>
2023-02-06 15:21:55 +0900

    feat : [013-ADD-CART] 카트에 대한 추가 구현

--- order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
+++ order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
 
 	ALREADY_EXIST_ITEM_NAME(HttpStatus.BAD_REQUEST, "중복된 아이템명입니다."),
 	PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST, "상품을 찾을 수 없습니다."),
-	ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "아이템을 찾을 수 없습니다.");
+	ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "아이템을 찾을 수 없습니다."),
+	ITEM_COUNT_NOT_ENOUGH(HttpStatus.BAD_REQUEST, "상품 수량이 부족합니다."),
+	KART_CHANGE_FAIL(HttpStatus.BAD_REQUEST, "장바구니에 추가할 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String detail;
