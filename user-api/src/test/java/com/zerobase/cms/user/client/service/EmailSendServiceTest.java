1a3ee6d67591ebed40a73abea6068cc2f3943834
junga970 <junga2940@gmail.com>
2023-02-06 10:37:45 +0900

    Feat : [001-EMAIL-Connect] mailgun에 대한 email 연동
    무료계정으로 테스트 시 이메일 등록 반드시 필요.

---
+++ user-api/src/test/java/com/zerobase/cms/user/client/service/EmailSendServiceTest.java
@@ -0,0 +1,19 @@
+package com.zerobase.cms.user.client.service;
+
+import com.zerobase.cms.user.client.MailgunClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSendServiceTest {
+
+	@Autowired
+	private MailgunClient mailgunClient;
+
+	@Test
+	void test() {
+//		String response = emailSendService.sendEmail();
+//		System.out.println(response);
+	}
+}
