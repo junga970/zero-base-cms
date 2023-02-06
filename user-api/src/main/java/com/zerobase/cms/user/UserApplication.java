2485e1561eb808ee5ad1c9d878c084a722878069
junga970 <junga2940@gmail.com>
2023-02-06 11:49:24 +0900

    feat : [004-LOGIN-CUSTOMER] 고객 로그인 구현

--- user-api/src/main/java/com/zerobase/cms/user/UserApplication.java
+++ user-api/src/main/java/com/zerobase/cms/user/UserApplication.java
@@ -3,10 +3,12 @@ package com.zerobase.cms.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ServletComponentScan
 @EnableFeignClients
 @SpringBootApplication
 @EnableJdbcAuditing
